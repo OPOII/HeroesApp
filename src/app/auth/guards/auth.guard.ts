a65f9e7e5dedd39ab183bfb1d3d869fbb2c7b188
OPOII <bryancamili198@hotmail.com>
2023-07-17 10:03:18 -0500

    funcionalidad del guard

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,16 +1,28 @@
 
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivateFn, CanMatchFn, Route, RouterStateSnapshot, UrlSegment } from '@angular/router';
+import { inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivateFn, CanMatchFn, Route, Router, RouterStateSnapshot, UrlSegment } from '@angular/router';
+import { Observable, tap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 
+
+const checkAuthStatus=():boolean|Observable<boolean>=>{
+  const authService:AuthService=inject(AuthService);
+  const router:Router=inject(Router);
+
+  return authService.checkAuthentication().pipe(
+    tap(isAuthenticated=>console.log({'Authenticated':isAuthenticated})),
+    tap((isAuthenticated)=>{
+      if(!isAuthenticated){
+        router.navigate(['/auth/login'])
+      }
+    })
+  )
+}
 export const canMatchGuard:CanMatchFn=(route:Route,segments:UrlSegment[])=>{
-  console.log('Can match');
-  console.log({route,segments})
-  return false;
+  return checkAuthStatus();
 }
 export const canActivateGuard:CanActivateFn=(route:ActivatedRouteSnapshot,state:RouterStateSnapshot)=>{
 
-  console.log('Can match');
-  console.log({route,state})
-  return false;
+  return checkAuthStatus();
 }
